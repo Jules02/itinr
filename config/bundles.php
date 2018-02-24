cdd04314f9468e113c0699dc1faf42d9698ca365
Jules02 <julocssa@gmail.com>
2018-02-24 13:21:02 +0100

    Installation Encore (on peut désormais utiliser Asset pour gérer les CSS et JS) Exemple page apropos

--- config/bundles.php
+++ config/bundles.php
@@ -6,4 +6,7 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
